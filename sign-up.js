bc3a2fdf0b3756af67c6fc5acc504d88bb90c44e
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-10-27 00:12:40 +0300

    Superboss

--- sign-up.js
+++ sign-up.js
@@ -66,7 +66,7 @@ const onSubmit = async (event) => {
     const { ip } = await getIP();
 
     const body = JSON.stringify({
-      name: "nomad",
+      name: "superboss",
       email: formRef.email.value,
       browserId,
       ip,
